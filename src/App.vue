b43e053f7d64f7c40c2c8a52bc2bd5856dca4a3f
Runju-Raton <68098346+Runju-Raton@users.noreply.github.com>
2024-05-29 23:27:45 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,206 @@
+<script setup>
+import { ref } from 'vue';
+import moment from 'moment';
+
+const formData = ref({
+  title                : '',
+  content              : ''
+});
+
+//initial some posts start
+const posts = ref([
+  {
+    id                : 1,
+    title             : 'Post 1',
+    content           : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    likes             : 0,
+    comments          : [],
+    date              : '2024-05-24 11:00:00',
+    commentVisibility : false
+  },
+  {
+    id                : 2,
+    title             : 'Post 2',
+    content           : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    likes             : 0,
+    comments          : [],
+    date              : '2024-05-24 11:00:00',
+    commentVisibility : false
+  },
+  {
+    id                : 3,
+    title             : 'Post 3',
+    content           : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    likes             : 0,
+    comments          : [],
+    date              : '2024-05-24 11:00:00',
+    commentVisibility : false
+  }
+]);
+//initial some posts end
+
+// assign empty array to store comment for each post.
+const newComment      = ref([]);
+
+// increase like for individual post
+function increaseLikeCount(index){
+  posts.value[index].likes++;
+}
+
+// create post
+function createPost(){
+  posts.value.push({
+    id                : posts.value.length?posts.value[(posts.value.length)-1].id + 1 : 1, // id incremented by 1 with last post id, if there is no post then id is initial 1
+    title             : formData.value.title,
+    content           : formData.value.content,
+    likes             : 0,
+    comments          : [],
+    date              : moment().format('YYYY-MM-DD HH:mm:ss'),
+    commentVisibility : false
+  })
+
+  formData.value.title    = ''; //empty form title
+  formData.value.content  = ''; //empty form content
+}
+
+// delete post
+function deletePost(index){
+  posts.value.splice(index,1);
+}
+
+// create comment
+function createComment(index){
+  // if comment exists, then push into comment array
+  if(newComment.value[index]){
+      posts.value[index].comments.push({
+      id            : posts.value[index].comments.length?posts.value[index].comments[(posts.value[index].comments.length)-1].id + 1 : 1, // id incremented by 1 with post's last comment id, if there is no comment then id is initial 1
+      text          : newComment.value[index],
+      date          : moment().format('YYYY-MM-DD HH:mm:ss')
+    })
+  }
+  newComment.value[index] = '';  //empty comment
+}
+
+// delete comment
+function deleteComment(post,commentIndex){
+  post.comments.splice(commentIndex,1)
+}
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">BAT Blog</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              John Doe
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><a class="dropdown-item" href="#">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- create post start-->
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Create a New Post</h5>
+            <form @submit.prevent="createPost()">
+              <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input v-model="formData.title" type="text" class="form-control" id="title" placeholder="Enter the title">
+              </div>
+              <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea v-model="formData.content" class="form-control" id="content" rows="3" placeholder="Enter the content"></textarea>
+              </div>
+              <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- create post end-->
+
+  <!-- post list start -->
+  <div class="posts mt-4">
+    <div class="container">
+      <div class="nav nav-tabs mb-4">
+        <h3>Posts</h3>
+      </div>
+
+      <div class="row">
+        <!-- post v-for loop here -->
+        <div class="col-md-4" v-for="(post,index) in posts" :key="post.id">
+          <div class="card mb-4">
+            <div class="card-body">
+              <h5 class="card-title">{{ post.title }}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">{{ moment(post.date).fromNow() }}</h6>
+              <p class="card-text">{{ post.content }}</p>
+              <p class="card-text">
+                <small v-if="post.likes > 1">{{ post.likes }} Likes, </small>
+                <small v-else-if="post.likes == 1">{{ post.likes }} Like, </small>
+                <small v-else>No Likes, </small>
+                <small @click="post.commentVisibility=!post.commentVisibility" style="cursor: pointer;">{{ 
+                  post.comments.length==1 ? post.comments.length + ' Comment':
+                  post.comments.length>1 ? post.comments.length + ' Comments': 'No Comments'
+                }}</small>
+              </p>
+
+              <div class="comments mb-3">
+                <div class="comments-input d-flex mb-3">
+                  <input v-model="newComment[index]" @keydown.enter="createComment(index)" type="text" class="form-control form-control-sm me-2" placeholder="Write Comment">
+                  <button @click="createComment(index)" class="btn btn-sm btn-success"><i class="bi bi-send"></i></button>
+                </div>
+                <!-- comments section start -->
+                <!-- comment v-for loop here -->
+                <div v-if="post.commentVisibility" class="comment mb-3 ms-3" v-for="(comment,commentIndex) in posts[index].comments">
+                  <h6 class="card-title small">John Doe <span @click="deleteComment(posts[index],commentIndex)" class="text-danger float-end cursor-pointer">X</span></h6>
+                  <p class="card-subtitle mb-1 text-body-secondary small">{{ moment(comment.date).fromNow() }}</p>
+                  <p class="card-text small">{{ comment.text }}</p>
+                </div>
+                <hr class="my-2 very-low-opacity">
+              </div>
+              <!-- comments section end -->
+              <button class="btn btn-sm btn-primary" @click="increaseLikeCount(index)">Like</button> 
+              <button class="btn btn-sm btn-danger float-end" @click="deletePost(index)"><i class="bi bi-trash"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- post list end -->
+</template>
+
+<style scoped>
+.cursor-pointer {
+  cursor: pointer;
+}
+
+.very-low-opacity {
+  opacity: 0.1;
+}
+</style>
